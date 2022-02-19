15f643eab14446aa0edc7b8873d5976998de6726
gpfla3690 <gpfla3690@gmail.com>
2022-02-19 17:46:38 +0900

    http GET 통신

--- src/main/java/com/yhr/mini/HomeServlet.java
+++ src/main/java/com/yhr/mini/HomeServlet.java
@@ -6,33 +6,30 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-/**
- * Servlet implementation class HomeServlet
- */
 @WebServlet("/home")
 public class HomeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public HomeServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+    
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		String number = request.getParameter("number");
+		
+		System.out.println("Client's number is" + number);
+		
+		int times = Integer.parseInt(number) * 5;
+		
+		PrintWriter write = response.getWriter();
+		
+		write.println("the result of 5 times of number is" + times);
+		
+		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
